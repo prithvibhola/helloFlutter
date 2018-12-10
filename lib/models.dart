e6a8d251dd2bbb08e409aeed17abedfa54c08656
prithvi <prithvibhola08@gmail.com>
2018-12-10 11:11:51 +0530

    GithubUser model added to models file

---
+++ lib/models.dart
@@ -0,0 +1,59 @@
+class GithubUser {
+  final String login;
+  final int id;
+  final String nodeId;
+  final String avatarUrl;
+  final String url;
+  final String htmlUrl;
+  final String followersUrl;
+  final String followingUrl;
+  final String gistsUrl;
+  final String starredUrl;
+  final String subscriptionsUrl;
+  final String organizationsUrl;
+  final String reposUrl;
+  final String eventsUrl;
+  final String receivedEventsUrl;
+  final String type;
+  final int score;
+
+  GithubUser(
+      {this.login,
+      this.id,
+      this.nodeId,
+      this.avatarUrl,
+      this.url,
+      this.htmlUrl,
+      this.followersUrl,
+      this.followingUrl,
+      this.gistsUrl,
+      this.starredUrl,
+      this.subscriptionsUrl,
+      this.organizationsUrl,
+      this.reposUrl,
+      this.eventsUrl,
+      this.receivedEventsUrl,
+      this.type,
+      this.score});
+
+  factory GithubUser.fromJson(Map json) {
+    return GithubUser(
+        login: json['login'],
+        id: json['id'],
+        nodeId: json['node_id'],
+        avatarUrl: json['avatar_url'],
+        url: json['url'],
+        htmlUrl: json['html_url'],
+        followersUrl: json['followers_url'],
+        followingUrl: json['following_url'],
+        gistsUrl: json['gists_url'],
+        starredUrl: json['starred_url'],
+        subscriptionsUrl: json['subscriptions_url'],
+        organizationsUrl: json['organizations_url'],
+        reposUrl: json['repos_url'],
+        eventsUrl: json['events_url'],
+        receivedEventsUrl: json['received_events_url'],
+        type: json['type'],
+        score: json['score']);
+  }
+}
